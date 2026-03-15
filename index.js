d8614f8d1dcfcc716a92537fbf3ce6afd9447470
Natalia-Myshenkova <natamis6122@gmail.com>
2026-03-15 18:21:13 +0000

    Deploying to gh-pages from @ Natalia-Myshenkova/goit-js-hw-10@43fe3118fc0cfa257ee7c3d27d7b88378fd7a633 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/styles-ZBgjL6D5.js";import{f as e}from"./assets/vendor-BbSUbo7J.js";import"./assets/1-timer-FvmkupKr.js";import"./assets/2-snackbar-Cwwfbu36.js";e("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1});
+import"./assets/styles-ZBgjL6D5.js";import{f as e}from"./assets/vendor-BbSUbo7J.js";import"./assets/1-timer-FvmkupKr.js";import"./assets/2-snackbar-CWO16PkI.js";e("#datetime-picker",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1});
 //# sourceMappingURL=index.js.map

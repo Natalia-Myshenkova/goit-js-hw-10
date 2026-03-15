43fe3118fc0cfa257ee7c3d27d7b88378fd7a633
Natalia <natamis6122@gmail.com>
2026-03-15 20:20:51 +0200

    add new snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,39 +5,40 @@ import "izitoast/dist/css/iziToast.min.css";
 const delayInput = document.querySelector('.delay');
 const resolveRadio = document.querySelector('.resolve');
 const rejectRadio = document.querySelector('.reject');
-const createBtn = document.querySelector('.create-btn');
+const form = document.querySelector('.form');
 
-createBtn.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const delay = Number(delayInput.value);
 
   const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (resolveRadio.checked) {
-        resolve(delay); 
-      } else {
-        reject(delay);
-      }
-    }, delay);
+    if (resolveRadio.checked) {
+      setTimeout(() => resolve(`✅ Fulfilled promise in ${delay}ms`), delay);
+    } else if (rejectRadio.checked) {
+      setTimeout(() => reject(`❌ Rejected promise in ${delay}ms`), delay);
+    } else {
+      reject(" Please select an option");
+    }
   });
 
   promise
-    .then(delay => {
-      console.log(`✅ Fulfilled promise in ${delay}ms`);
+    .then(message => {
+      console.log(message);
       iziToast.show({
-        message: `✅ Fulfilled promise in ${delay}ms`,
+        message: message,
         position: 'topRight',
         color: '#59a10d'
       });
     })
-    .catch(delay => {
-      console.log(`❌ Rejected promise in ${delay}ms`);
+    .catch(message => {
+      console.log(message);
       iziToast.show({
-        message: `❌ Rejected promise in ${delay}ms`,
+        message: message,
         position: 'topRight',
         color: '#ef4040'
       });
     });
-    document.querySelector('.form').reset();
+
+  form.reset();
 });
